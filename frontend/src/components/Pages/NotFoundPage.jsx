3f89c7df361296e619f0f229334d32132e72a249
Dmitrii Chernov <dm.a.chernov@gmail.com>
2023-07-17 13:08:55 +0300

    modify markup

--- frontend/src/components/Pages/NotFoundPage.jsx
+++ frontend/src/components/Pages/NotFoundPage.jsx
@@ -6,16 +6,14 @@ import {
 function NotFoundPage() {
   return (
     <Container className="h-100" fluid>
-      <Row className="justify-content-center align-items-center">
+      <Row className="h-100 justify-content-center align-items-center">
         <Col md="6" xxl="6">
           <Card className="my-5 py-5 h-100 shadow justify-items-center align-items-center">
             <Card.Body>
               <Card.Title className="text-center">
                 <h1>404</h1>
               </Card.Title>
-              <Card.Text className="text-center">
-                <h2>Страница не найдена</h2>
-              </Card.Text>
+              <h2>Страница не найдена</h2>
               <hr />
               <Card.Text className="text-center">
                 Вернуться на
