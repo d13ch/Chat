e0f8fc802a81e5b20d9dadafa8bb92ed48cb32ff
Dmitrii Chernov <dm.a.chernov@gmail.com>
2023-08-08 10:57:52 +0300

    update login form

--- frontend/src/components/pages/LoginPage/LoginForm/LoginForm.jsx
+++ frontend/src/components/pages/LoginPage/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -15,78 +15,74 @@ const LoginForm = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
 
-  const SignupSchema = Yup.object().shape({
+  const validationSchema = Yup.object().shape({
     username: Yup.string().required(t('errors.reqiredField')),
     password: Yup.string().required(t('errors.reqiredField')),
   });
 
-  return (
-    <Formik
-      initialValues={{
-        username: '',
-        password: '',
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={async (values, actions) => {
-        try {
-          const { data } = await axios.post(routes.loginPath(), {
-            username: values.username,
-            password: values.password,
-          });
-          actions.setSubmitting(false);
-          logIn(data);
-          navigate('/');
-        } catch (error) {
-          console.log(error);
-          if (error.message === 'Network Error') {
-            notify('error', t('toasts.networkError'));
-          }
-          if (error.isAxiosError && error.response.status === 401) {
-            setAuthFailed(true);
-          } else {
-            notify('error', t('toasts.unknownError'));
-          }
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        const { data } = await axios.post(routes.loginPath(), {
+          username: values.username,
+          password: values.password,
+        });
+        logIn(data);
+        navigate('/');
+      } catch (error) {
+        console.log(error);
+        if (error.message === 'Network Error') {
+          notify('error', t('toasts.networkError'));
         }
-      }}
-    >
-      {({ values, handleChange, handleSubmit }) => (
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mt-3">
-            <Form.Label htmlFor="username" className="ms-2 fw-semibold">{t('loginPage.username')}</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder={t('loginPage.usernamePlaceholder')}
-              name="username"
-              id="username"
-              value={values.username}
-              onChange={handleChange}
-              isInvalid={authFailed}
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mt-3">
-            <Form.Label htmlFor="password" className="ms-2 fw-semibold">{t('loginPage.password')}</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder={t('loginPage.passwordPlaceholder')}
-              name="password"
-              id="password"
-              value={values.password}
-              onChange={handleChange}
-              isInvalid={authFailed}
-              required
-            />
-            <Form.Control.Feedback className="ps-2" type="invalid">{t('loginPage.wrongInputFeedback')}</Form.Control.Feedback>
-          </Form.Group>
-          <div className="d-flex justify-content-end">
-            <Button className="col-4 mt-4" variant="primary" type="submit">
-              {t('loginPage.enterBtn')}
-            </Button>
-          </div>
-        </Form>
-      )}
-    </Formik>
+        if (error.isAxiosError && error.response.status === 401) {
+          setAuthFailed(true);
+        } else {
+          notify('error', t('toasts.unknownError'));
+        }
+      }
+    },
+  });
 
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Group className="mt-3">
+        <Form.Label htmlFor="username" className="ms-2 fw-semibold">{t('loginPage.username')}</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder={t('loginPage.usernamePlaceholder')}
+          name="username"
+          id="username"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          isInvalid={authFailed}
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mt-3">
+        <Form.Label htmlFor="password" className="ms-2 fw-semibold">{t('loginPage.password')}</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder={t('loginPage.passwordPlaceholder')}
+          name="password"
+          id="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          isInvalid={authFailed}
+          required
+        />
+        <Form.Control.Feedback className="ps-2" type="invalid">{t('loginPage.wrongInputFeedback')}</Form.Control.Feedback>
+      </Form.Group>
+      <div className="d-flex justify-content-end">
+        <Button className="col-4 mt-4" variant="primary" type="submit">
+          {t('loginPage.enterBtn')}
+        </Button>
+      </div>
+    </Form>
   );
 };
 
