2883502a2bb72b43cbf5fa8a15125137a3bdc898
Dmitrii Chernov <dm.a.chernov@gmail.com>
2023-08-10 16:57:29 +0300

    add auto scroll to bottom

--- frontend/src/components/pages/MainPage/components/MessagesBox.jsx
+++ frontend/src/components/pages/MainPage/components/MessagesBox.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Stack } from 'react-bootstrap';
 
-const MessagesBox = ({ messages }) => (
-  <Stack className="h-100 ps-4 mb-3 overflow-auto">
-    {messages.map((message) => (
-      <div className="text-break" key={message.id}>
-        <b>{message.username}</b>
-        :&nbsp;
-        {message.body}
-      </div>
-    ))}
-  </Stack>
-);
+const MessagesBox = ({ messages }) => {
+  const boxRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      boxRef.current.lastElementChild?.scrollIntoView({ behavior: 'smooth' });
+    }, 0);
+  }, [messages]);
+
+  return (
+    <Stack ref={boxRef} className="h-100 ps-4 mb-3 overflow-auto">
+      {messages.map((message) => (
+        <div className="text-break" key={message.id}>
+          <b>{message.username}</b>
+          :&nbsp;
+          {message.body}
+        </div>
+      ))}
+    </Stack>
+  );
+};
 
 export default MessagesBox;
