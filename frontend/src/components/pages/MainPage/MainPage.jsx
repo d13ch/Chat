adf3cb6ae0bf7ea3ce01f8abbc66def9dee8f802
Dmitrii Chernov <dm.a.chernov@gmail.com>
2023-08-10 16:58:33 +0300

    handle error of invalid token and add spinner

--- frontend/src/components/pages/MainPage/MainPage.jsx
+++ frontend/src/components/pages/MainPage/MainPage.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  Col, Container, Row, Spinner,
+} from 'react-bootstrap';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
@@ -9,10 +11,13 @@ import routes from '../../../routes/index.js';
 import { addChannels, setActiveChannel, setDefaultChannel } from '../../../slices/channelsSlice.js';
 import { addMessages } from '../../../slices/messagesSlice.js';
 import notify from '../../notifications/notify.js';
+import AuthContext from '../../../contexts/AuthContext.jsx';
 
 const MainPage = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const { logOut } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getChannels = async () => {
@@ -30,10 +35,17 @@ const MainPage = () => {
         dispatch(setDefaultChannel(currentChannelId));
         dispatch(addChannels(channels));
         dispatch(addMessages(messages));
+
+        setIsLoading(false);
       } catch (error) {
+        setIsLoading(false);
         console.log(error);
         if (error.message === 'Network Error') {
           notify('error', t('toasts.networkError'));
+        }
+        if (error.isAxiosError && error.response.status === 401) {
+          notify('error', t('toasts.authRequired'));
+          logOut();
         } else {
           notify('error', t('toasts.unknownError'));
         }
@@ -44,16 +56,24 @@ const MainPage = () => {
   });
 
   return (
-    <Container className="shadow h-100 px-1 my-4 overflow-hidden">
-      <Row className="h-100 bg-white">
-        <Col md="3" xxl="2" className="h-100 d-flex flex-column col-4 ps-3 pe-2 bg-light">
-          <ChannelsPanel />
-        </Col>
-        <Col className="h-100 pe-3">
-          <MessagesPanel />
-        </Col>
-      </Row>
-    </Container>
+    isLoading
+      ? (
+        <div className="d-flex justify-content-center align-items-center h-100">
+          <Spinner animation="border" variant="primary" />
+        </div>
+      )
+      : (
+        <Container className="shadow h-100 px-1 my-4 overflow-hidden">
+          <Row className="h-100 bg-white">
+            <Col md="3" xl="2" className="h-100 d-flex flex-column col-4 ps-3 pe-2 bg-light">
+              <ChannelsPanel />
+            </Col>
+            <Col className="h-100 pe-3">
+              <MessagesPanel />
+            </Col>
+          </Row>
+        </Container>
+      )
   );
 };
 
